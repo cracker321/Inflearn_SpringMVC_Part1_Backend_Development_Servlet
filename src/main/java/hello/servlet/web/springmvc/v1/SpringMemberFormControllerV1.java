3accb82b82f366cf7e6feae17941d5103f7bc2a2
yujong <yujong4170@gmail.com>
2023-01-13 13:45:41 +0900

    yujong

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberFormControllerV1.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
-@Controller
+@Controller //
+
+
 public class SpringMemberFormControllerV1 {
 
 
