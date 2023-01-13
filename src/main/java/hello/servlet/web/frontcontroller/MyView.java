58712981f180b76e12547cb65a032abd11bb6700
yujong <yujong4170@gmail.com>
2023-01-13 15:58:48 +0900

    yujong

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -11,12 +11,12 @@ public class MyView {
     private String viewPath;
 
     public MyView(String viewPath) {
-
         this.viewPath = viewPath;
     }
 
     public void render(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
 
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
@@ -25,9 +25,10 @@ public class MyView {
     public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-
         modelToRequestAttribute(model, request);
+
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+
         dispatcher.forward(request, response);
     }
 
--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/FrontControllerServletV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/FrontControllerServletV3.java
@@ -1,8 +1,9 @@
 package hello.servlet.web.frontcontroller.v3.controller;
 
 
+import hello.servlet.web.frontcontroller.ModelView;
 import hello.servlet.web.frontcontroller.MyView;
-import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,15 +15,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-@WebServlet(name = "frontControllerServletV2", urlPatterns = "/frontcontroller/v2/*")
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/frontcontroller/v3/*")
 public class FrontControllerServletV3 extends HttpServlet {
-    private Map<String, ControllerV2> controllerMap = new HashMap<>();
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
 
     public FrontControllerServletV3() {
-        controllerMap.put("/front-controller/v2/members/new-form", new MemberFormControllerV3());
-        controllerMap.put("/front-controller/v2/members/save", new MemberSaveControllerV3());
-        controllerMap.put("/front-controller/v2/members", new MemberListControllerV3());
-
+        controllerMap.put("/front-controller/v3/members/new-form", new
+                MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new
+                MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new
+                MemberListControllerV3());
     }
 
     @Override
@@ -31,14 +34,35 @@ public class FrontControllerServletV3 extends HttpServlet {
 
 
         String requestURI = request.getRequestURI();
-        ControllerV2 controller = controllerMap.get(requestURI);
+        ControllerV3 controller = controllerMap.get(requestURI);
 
-        if (controller == null) {response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
 
             return;
         }
 
-        MyView view = controller.process(request, response);
-        view.render(request, response);
+        Map<String, String> paramMap = createParamMap(request);
+
+        ModelView mv = controller.process(paramMap);
+
+        String viewName = mv.getViewName();
+
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName,
+                        request.getParameter(paramName)));
+        return paramMap;
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
