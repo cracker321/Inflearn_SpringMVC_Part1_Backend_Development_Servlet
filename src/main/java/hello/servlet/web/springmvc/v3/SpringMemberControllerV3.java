19185030354c05388eb3f0b3860c5c87a0232d34
yujong <yujong4170@gmail.com>
2023-01-13 14:54:51 +0900

    yujong

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -48,7 +48,7 @@ public class SpringMemberControllerV3 {
 
     
     @GetMapping
-    public String members(Model model) {
+    public Sodel model)tring members(Model model) {
 
         List<Member> members = memberRepository.findAll();
         model.addAttribute("members", members);
