f470ab633f27f005894876209c82b87ff9769eb9
yujong <yujong4170@gmail.com>
2023-01-13 14:59:46 +0900

    yujong

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -5,10 +5,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,13 +22,15 @@ import java.util.List;
 public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @GetMapping("/new-form")
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET) 이렇게 쓰는 대신 아래에 '@GetMapping'을 슨다
+    @GetMapping
     public String newForm() {
-
 
         return "new-form";
     }
 
+
+    //@RequestMapping(value = "/save", method = RequestMethod.Post) 이렇게 쓰는 대신 아래에 '@PostMapping'을 슨다
     @PostMapping("/save")
     public String save(@RequestParam("username") String username,
                        @RequestParam("age") int age, Model model) {
